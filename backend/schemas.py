3ae38580d000052509db7c5c0f6c8dcb3c71f660
cmpbj <magnocarlos3232@gmail.com>
2025-01-16 08:35:04 -0300

    Add database, model, schema and controller files

---
+++ backend/schemas.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, PositiveFloat, EmailStr, Field
+from datetime import datetime
+from typing import Optional
+
+class ProductBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: PositiveFloat
+    categoria: str
+    email_fornecedor: EmailStr
+
+class ProductCreate(ProductBase):
+    pass
+
+class ProductResponse(ProductBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class ProductUpdate(ProductBase):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[PositiveFloat] = None
+    categoria: Optional[str] = None
+    email_fornecedor: Optional[EmailStr] = None
